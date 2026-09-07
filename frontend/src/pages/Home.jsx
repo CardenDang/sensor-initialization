b9e9742b5d7a88c8370e74badf42c9e62eacf601
Carden Dang <dangcarden@gmail.com>
2026-09-07 13:50:35 -0700

    Setup frontpage structure

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,15 +4,20 @@ import "../styles/styling.css";
 function Home() {
     return(
         <main className="home">
-            {/* Homepage will have data visualizations, graphs, charts, ML model results for optimal sleeping results */}
+            {/* Homepage will have data visualizations and graphs */}
             <h1 className="header">
                 Sleep Quality Monitor
             </h1>
 
             {/* Data page will display raw data, similar to Excel file */}
-            <section className="data">
-                <Link to="/data" className="data.btn">
+            <section className="home-sections">
+                <Link to="/data" className="data-btn">
                     Data
+                </Link>
+
+            {/* Dashboard page has ML model results, optimal sleep results, suggwstions, and overview of user's sleep */}
+                <Link to="/dashboard" className="dashboard-btn">
+                    Dashboard
                 </Link>
             </section>
         </main>
